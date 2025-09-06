d7399cfec72ddbab59a82eb313a3ed2579a12ee8
LoganSu <logansu1025@outlook.com>
2025-09-05 21:22:14 -0500

    Add create listing api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose"
 import dotenv from "dotenv"
 import userRouter from "../api/routes/user.route.js"
 import authRouter from "../api/routes/auth.route.js"
+import listingRouter from "../api/routes/listing.route.js"
 import cookieParser from "cookie-parser"
 
 dotenv.config()
@@ -30,6 +31,7 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRouter)
 app.use("/api/auth", authRouter)
+app.use("/api/listing", listingRouter)
 
 // Middleware
 app.use((err, req, res, next) => {
