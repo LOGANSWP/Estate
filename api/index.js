ea874718ffc8400e774c0b0e309a91761336d2e1
LoganSu <logansu1025@outlook.com>
2025-09-29 15:01:33 -0500

    I think it could fix the error

--- api/index.js
+++ api/index.js
@@ -38,6 +38,10 @@ app.use("/api/listing", listingRouter)
 
 app.use(express.static(path.join(__dirname, "/client/dist")))
 
+app.get(/(.*)/, (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
+
 // Middleware
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500
